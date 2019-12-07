65ac6914bcf9ff6b0ecd59ede9f673ce5456bb7d
Jim hardwicke <jim.hardwicke@accantosystems.com>
2019-12-07 10:08:53 +0000

    reshaped target-service to include adoption assembly

--- load.sh
+++ load.sh
@@ -1,4 +1,4 @@
 
 cd legacy-service; lmctl project $1 $2 ; cd ..
 cd target-service; lmctl project $1 $2 ; cd ..
-cd target-service-managed; lmctl project $1 $2 ; cd ..
+#cd target-service-managed; lmctl project $1 $2 ; cd ..
